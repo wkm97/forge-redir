c7867605bb3853dac3e45cb37fe8b7bc6183b747
karming.wan <karming.wan@servicerocket.com>
2022-03-25 13:54:20 +0800

    Revert "conditional render configuration"
    This reverts commit db8029214d661d6ce02e2575542d5913f8c8e9c5.

--- src/config.tsx
+++ src/config.tsx
@@ -1,73 +1,43 @@
 
 import api, { route } from "@forge/api";
 import {Content, Space} from '@servicerocket/conf-cloud-utils';
-import ForgeUI, { TextField, MacroConfig, render, Select, Option, useConfig, useProductContext, useEffect, CheckboxGroup, Checkbox, Fragment } from "@forge/ui";
+import ForgeUI, { TextField, MacroConfig, render, Select, Option, useConfig, useProductContext, useEffect, CheckboxGroup, Checkbox } from "@forge/ui";
 import { useState } from "@forge/ui";
 import { getAllSpaces, getContentsBySpaceKey } from "./confluence";
 
-const InternalConfiguration = () => {
+const Configuration = () => {
   const config = useConfig();
   const context = useProductContext();
   const [spaces, setSpaces] = useState<Space[]>(null);
   const [contentOptions, setContentOptions] = useState<Content[]>(null);
+
+
   useEffect(async()=>{
     setSpaces(await getAllSpaces());
   }, [])
 
   useEffect(async()=>{
-    const spaceKey = config?.selectedSpace || context.spaceKey
-    const pageContentOptions = await getContentsBySpaceKey(spaceKey, "page")
-    const blogContentOptions = await getContentsBySpaceKey(spaceKey, "blogpost")
-    const contentOptions = [...pageContentOptions, ...blogContentOptions]
-    setContentOptions(contentOptions)
+    setContentOptions(await getContentsBySpaceKey(config?.selectedSpace || context.spaceKey))
   }, [config])
 
   return (
-    <Fragment>
+    <MacroConfig>
       <Select label="space" name="selectedSpace">
         {spaces?.map(space=>
           context.spaceKey === space.key ? <Option label={space.name} value={space.key} defaultSelected/>
-          : <Option key={space.key} label={space.name} value={space.key}/>
+          : <Option label={space.name} value={space.key}/>
         )}
       </Select>
       <Select label="content" name="selectedContent">
         {contentOptions?.map(contentOption=>
-          <Option key={contentOption.id} label={contentOption.title} value={contentOption.id}/>
+          <Option label={contentOption.title} value={contentOption.id}/>
         )}
       </Select>
-    </Fragment>
-  )
-}
-
-const Configuration = () => {
-  const config = useConfig();
-  console.log(config)
-
-  const renderConfig = (type: string) => {
-    return (
-      <Fragment>
-        {type === "external"?  <TextField isRequired={true} label="link" name="link" type="text"/> : null}
-        {type === "internal"? <InternalConfiguration/>:null}
-      </Fragment>
-    )
-  }
-
-  return (
-    <MacroConfig>
-      <Select label="type" name="type">
-        <Option label="External Link" value="external"/>
-        <Option label="Internal Link" value="internal"/>
-      </Select>
-      {config? 
-      <Fragment>
-        {renderConfig(config.type)}
-        <TextField isRequired={true} label="countdown" name="countdown" type="number"/>
-        <CheckboxGroup label='options' name='options'>
-          <Checkbox defaultChecked value='visible' label='visible' />
-          <Checkbox defaultChecked value='redirectable' label='redirectable' />
-        </CheckboxGroup>
-      </Fragment>
-      : null}
+      <TextField isRequired={true} label="countdown" name="countdown"/>
+      <CheckboxGroup label='options' name='options'>
+        <Checkbox defaultChecked value='visible' label='visible' />
+        <Checkbox defaultChecked value='redirectable' label='redirectable' />
+      </CheckboxGroup>
     </MacroConfig>
   )
 }
--- src/confluence.ts
+++ src/confluence.ts
@@ -2,13 +2,12 @@
 import api, { route } from "@forge/api";
 import {Content, Space} from '@servicerocket/conf-cloud-utils';
 
-export const getContentsBySpaceKey = async(spaceKey: string, type: "page" | "blogpost"): Promise<Content[]> => {
+export const getContentsBySpaceKey = async(spaceKey: string): Promise<Content[]> => {
   if(!spaceKey){
     throw new Error('SpaceKey not available')
   }
   const queryParams = new URLSearchParams({
-    spaceKey,
-    type
+    spaceKey
   });
 
   const response = await (await api.asUser().requestConfluence(route`/wiki/rest/api/content?${queryParams}`)).json()
